8992305d06881d4f64f1d63a72c1f0dd75de8e85
Anish Adhikari <anish531213@gmail.com>
2018-02-20 20:22:12 -0500

    Initial Commit

---
+++ Instagram-01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Instagram-01
+//
+//  Created by Anish Adhikari on 2/20/18.
+//  Copyright © 2018 Anish Adhikari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
